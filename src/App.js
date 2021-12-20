b562905c40ec323128a002620e4e51c48f854d32
SrSauloss <saulosilva@super.ufam.edu.br>
2021-12-20 14:47:30 -0400

    Added layout desktop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./reset.css";
 import "./App.css";
+import Home from "./pages/home/index.js";
 
 function App() {
   return (
-    <div>
-      <h1>Check</h1>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        {/*<Route path="/send-test" exact>
+          <SendTest />
+        </Route>
+        <Route path="/professors" exact>
+          <Professors />
+        </Route>
+        <Route path="/subjects" exact>
+          <Subjects />
+  </Route>*/}
+      </Switch>
+    </BrowserRouter>
   );
 }
 
